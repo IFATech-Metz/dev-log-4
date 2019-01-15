22b41d998819cda6a87abe56c1a4a0a21a6bf2a7
stephanieremy <stephanie_remy@yahoo.fr>
2019-01-15 17:10:50 +0100

    Prevision actuelle

--- meteo.js
+++ meteo.js
@@ -6,20 +6,63 @@ var xhr = new XMLHttpRequest();
 
 var base_url = "http://api.openweathermap.org/data/2.5/weather";
 var city = "Metz";
-var appid = "3c084bd74c2f77f02d6d6c30c2018bf0";
+var units = "metric";
+var appid = "f5e810531af1756846022c6f387acf25";
 
 function get_url() {
     return base_url + "?"
         + "q=" + city + "&"
+        + "units=" + units + "&"
         + "appid=" + appid;
+        
 }
 
 function init_page() {
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("demo").innerHTML = this.responseText;
+        
+            document.getElementById("url").innerHTML = get_url();
+//Temperature
+            var response = JSON.parse(this.responseText);
+            var temperature = response.main.temp;
+//Icone
+            var icon = response.weather[0].icon;
+            var src = "http://openweathermap.org/img/w/" + icon + ".png";
+
+
+            document.getElementById("meteo").innerHTML = temperature;
+            document.getElementById("icon").src = src;
         }
-    };
+    }
+    
+    xhr.open("GET", get_url(), true)
+    xhr.send()
+}
+function get_temperature() {
+    city = document.getElementById("ville").value;
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            //TODO
+            document.getElementById("url").innerHTML = get_url();
+
+            if(document.getElementById("url_visibility").checked){
+                document.getElementById("url").style.display = "block";
+            }
+            else{
+                document.getElementById("url").style.display = "none";
+            }
+
+            var response = JSON.parse(this.responseText);
+            var temperature = response.main.temp;
+
+            var icon = response.weather[0].icon;
+            var src = "http://openweathermap.org/img/w/" + icon + ".png";
+
+            document.getElementById("meteo").innerHTML = temperature;
+            document.getElementById("icon").src = src;
+        }
+    }
     
     xhr.open("GET", get_url(), true)
     xhr.send()
